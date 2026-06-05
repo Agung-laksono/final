b0f2eecd88fc286a0df84257d9b649e7c6350717
agung laskono <agunglak1994@gmail.com>
2026-06-05 10:57:44 +0700

    membuat halaman index

---
+++ Modules/Inventory/resources/views/livewire/index.blade.php
@@ -0,0 +1,5 @@
+<x-inventory::layouts.master>
+    <div>
+        <h1>Inventory</h1>
+    </div>
+</x-inventory::layouts.master>
--- Modules/Inventory/routes/web.php
+++ Modules/Inventory/routes/web.php
@@ -4,6 +4,15 @@ use Illuminate\Support\Facades\Route;
 use Modules\Inventory\Http\Controllers\InventoryController;
 use Livewire\Volt\Volt;
 
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::resource('inventories', InventoryController::class)->names('inventory');
-});
+Route::middleware(['auth', 'verified'])
+    ->prefix('inventory') // <-- Ini yang membuat URL-nya menjadi domain.com/inventory
+    ->name('inventory.')  // <-- Ini penamaan otomatis untuk route (misal: route('inventory.index'))
+    ->group(function () {
+        
+        // 1. Route untuk Halaman Utama Inventory (domain.com/inventory)
+        // Pastikan Anda sudah membuat file 'index.blade.php' di Modules/Inventory/resources/views/livewire/
+        Volt::route('/', 'index')->name('index');
+        // (Opsional) Jika nanti Anda membuat halaman daftar barang (domain.com/inventory/barang)
+        // Volt::route('/barang', 'barang-list')->name('barang');
+        
+    });
