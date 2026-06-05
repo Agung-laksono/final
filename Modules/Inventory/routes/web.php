030f7d679e5ce047508ff80a1094d8515087a324
agung laskono <agunglak1994@gmail.com>
2026-06-05 10:43:51 +0700

    fix: konfigurasi composer autoload untuk laravel modules

--- Modules/Inventory/routes/web.php
+++ Modules/Inventory/routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use Modules\Inventory\Http\Controllers\InventoryController;
+use Livewire\Volt\Volt;
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::resource('inventories', InventoryController::class)->names('inventory');
