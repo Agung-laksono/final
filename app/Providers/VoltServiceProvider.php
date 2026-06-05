5aad00f02174d82021a2145a153f8ed4a3c14cad
agung laskono <agunglak1994@gmail.com>
2026-06-05 10:33:28 +0700

    feat: konfigurasi livewire volt untuk laravel modules

--- app/Providers/VoltServiceProvider.php
+++ app/Providers/VoltServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Livewire\Volt\Volt;
+use Nwidart\Modules\Facades\Module;
 
 class VoltServiceProvider extends ServiceProvider
 {
@@ -24,5 +25,24 @@ class VoltServiceProvider extends ServiceProvider
             config('livewire.view_path', resource_path('views/livewire')),
             resource_path('views/pages'),
         ]);
+
+        // Memberitahu Volt agar ikut memantau dan mencari file komponen di dalam folder setiap Modul
+        // Looping semua modul yang statusnya aktif
+        foreach (Module::allEnabled() as $module) {
+            $livewirePath = module_path($module->getName(), 'resources/views/livewire');
+            $pagesPath = module_path($module->getName(), 'resources/views/pages');
+            // Kita cek dulu foldernya ada atau tidak agar tidak terjadi error
+            $paths = [];
+            if (is_dir($livewirePath)) {
+                $paths[] = $livewirePath;
+            }
+            if (is_dir($pagesPath)) {
+                $paths[] = $pagesPath;
+            }
+            // Jika foldernya ada, beritahu Volt untuk memantaunya
+            if (!empty($paths)) {
+                Volt::mount($paths);
+            }
+        }
     }
 }
