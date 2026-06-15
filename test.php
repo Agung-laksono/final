72d44bbd168fb91d674fc2beff6a757c5a04c8f6
agung-laksono <agunglak1994@gmail.com>
2026-06-15 22:42:13 +0700

    deleter file testing

--- test.php
+++
@@ -1,6 +0,0 @@
-<?php
-require 'vendor/autoload.php';
-$app = require_once 'bootstrap/app.php';
-$app->make(\Illuminate\Contracts\Console\Kernel::class)->bootstrap();
-$c = app('livewire')->new('item-history-movement.movement-list');
-dump(\Livewire\Drawer\Utils::getPublicMethodsDefinedBySubClass($c));
