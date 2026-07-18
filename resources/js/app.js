6621b35494ae8eafe9337d6e65bde3db5d7b498f
agung laskono <agunglak1994@gmail.com>
2026-07-18 20:55:54 +0700

    fix: correctly close alpine.data in app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -449,7 +449,7 @@ const initCropper = () => {
                 if (bytes === 0) return '0 KB';
                 return (bytes / 1024).toFixed(1) + ' KB';
             }
-        });
+    }));
 };
 
 if (window.Alpine) {
