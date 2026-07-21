fb7631071a80fadfacd6726fec63f52ade802d7a
agung-laksono <agunglak1994@gmail.com>
2026-07-21 12:51:01 +0700

    feat: implement service worker with push notification support and update build assets

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,7 @@
 // Pusher Beams - WAJIB ada di baris pertama agar push notification bekerja
 importScripts('https://js.pusher.com/beams/service-worker.js');
 
-const CACHE_NAME = 'inventory-pwa-cache-v1784605717718';
+const CACHE_NAME = 'inventory-pwa-cache-v1784606447275';
 const urlsToCache = [
     '/',
     '/manifest.json',
