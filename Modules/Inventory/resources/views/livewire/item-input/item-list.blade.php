6ea604f243bdb1ca63063cff707ab9e44305670d
agung laskono <agunglak1994@gmail.com>
2026-08-14 07:39:20 +0700

    feat: add livewire volt item management page with search, filtering, and real-time pusher updates

--- Modules/Inventory/resources/views/livewire/item-input/item-list.blade.php
+++ Modules/Inventory/resources/views/livewire/item-input/item-list.blade.php
@@ -334,10 +334,23 @@ $delete = function (Item $item) {
                     </button>
                     
                     @can('inventory.item.create')
-                        <div x-show="!$store.catalog.selectionMode" x-cloak class="shrink-0">
-                            <flux:button x-on:click="$wire.dispatch('open-item-modal')" variant="primary" icon="plus">
+                        <div x-data="{ isModalLoading: false }" 
+                             @do-open-item-modal.window="isModalLoading = false" 
+                             @item-modal-loaded.window="isModalLoading = false"
+                             x-show="!$store.catalog.selectionMode" 
+                             x-cloak 
+                             class="shrink-0">
+                            <button x-on:click="isModalLoading = true; $wire.dispatch('open-item-modal')" 
+                                    x-bind:disabled="isModalLoading" 
+                                    class="bg-indigo-600 hover:bg-indigo-500 active:bg-indigo-700 disabled:opacity-50 disabled:cursor-not-allowed text-white px-3 py-2 rounded-lg text-sm font-semibold transition-colors flex items-center gap-2">
+                                <template x-if="!isModalLoading">
+                                    <flux:icon.plus class="w-4 h-4" />
+                                </template>
+                                <template x-if="isModalLoading">
+                                    <flux:icon.arrow-path class="w-4 h-4 animate-spin" />
+                                </template>
                                 <span class="hidden sm:inline">Barang</span>
-                            </flux:button>
+                            </button>
                         </div>
                     @endcan
                 </div>
