1f565c559efc7937b446711e529d2e8377886b00
agung-laksono <agunglak1994@gmail.com>
2026-09-17 09:53:28 +0700

    chore: add production build assets and service worker

--- public/sw.js
+++ public/sw.js
@@ -5,7 +5,7 @@ try {
     console.warn('[SW] Pusher Beams diblokir atau gagal dimuat:', e);
 }
 
-const CACHE_NAME = 'inventory-pwa-cache-v1788841725521';
+const CACHE_NAME = 'inventory-pwa-cache-v1789613593876';
 const urlsToCache = [
     '/',
     '/manifest.json',
